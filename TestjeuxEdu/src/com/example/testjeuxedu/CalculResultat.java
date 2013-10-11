0f1bb8d49340c76bb5b5026d0c5e9570a67cc212
Faisal <Faisal@Faisal-PC>
2013-10-11 16:14:33 +0200

    jeux calcul fonctionnel

---
+++ TestjeuxEdu/src/com/example/testjeuxedu/CalculResultat.java
@@ -0,0 +1,25 @@
+package com.example.testjeuxedu;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.widget.TextView;
+
+public class CalculResultat extends Activity {
+private int cptBonneRep;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_calcul_resultat);
+		TextView rep = (TextView) findViewById(R.id.rep);
+		
+		cptBonneRep = this.getIntent().getExtras().getInt("cptBonneRep");
+		String bonneRep = String.valueOf(cptBonneRep);
+		
+		rep.setText(bonneRep+" bonnnes réponse !!!");
+		
+		
+	}
+
+
+}
