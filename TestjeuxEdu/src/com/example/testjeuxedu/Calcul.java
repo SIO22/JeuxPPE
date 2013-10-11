1f23622e8e0c152abeaadeb5866eb8eed2af3ab5
Faisal <Faisal@172.17.253.183>
2013-10-11 11:30:09 +0200

    -jeux calcul fonctionne

--- TestjeuxEdu/src/com/example/testjeuxedu/Calcul.java
+++ TestjeuxEdu/src/com/example/testjeuxedu/Calcul.java
@@ -1,27 +1,63 @@
 package com.example.testjeuxedu;
 
+import java.util.Random;
+
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class Calcul extends Activity {
-	TextView calcul = null;
+public class Calcul extends Activity implements OnClickListener {
+	TextView calcul;
+	private Button calculer;
+	private String valTappe;
+	private String valeurFinal2;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_calcul);
+
+		calcul = (TextView) findViewById(R.id.calcul);
+		calculer = (Button) findViewById(R.id.calculer);
 		
+		Random r = new Random();
+
+		int valeur = 0 + r.nextInt(9 - 0);
+		int valeur2 = 0 + r.nextInt(9 - 0);
+		int valeurFinal = valeur + valeur2;
 		
-		calcul=(TextView)findViewById(R.id.calcul); 
-	    calcul.setText("Step One: blast egg");
+		String test = String.valueOf(valeur) + "+" + String.valueOf(valeur2)+ " =";		
+		
+		valeurFinal2 = String.valueOf(valeurFinal);
+		
+		calcul.setText(test);
+		calculer.setOnClickListener(this);
+		Log.i("chiffreatrouvéonCreate", " " + valeurFinal2);
 	}
 
+
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.calcul, menu);
-		return true;
+	public void onClick(View v) {
+		EditText chiffreTap = (EditText) findViewById(R.id.chiffreTap);
+		valTappe = chiffreTap.getText().toString();
+		
+		if (v == calculer) {
+			if (Integer.parseInt(valTappe) == Integer.parseInt(valeurFinal2)) {
+				Toast toast1 = Toast.makeText(getApplicationContext(),
+						"Hello toast!", Toast.LENGTH_SHORT);
+				toast1.show();
+			} else {
+				Toast toast2 = Toast.makeText(getApplicationContext(), "Fail",
+						Toast.LENGTH_SHORT);
+				toast2.show();
+			}
+		}
 	}
 
 }
